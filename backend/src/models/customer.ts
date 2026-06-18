d769a2e17f3bcffeb58810e360977ebb2f26715d
Joao victor <joaoparronchi04@gmail.com>
2026-06-18 13:03:09 -0300

    trocando prisma por mongoose

---
+++ backend/src/models/customer.ts
@@ -0,0 +1,40 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface ICustomer extends Document {
+    name: string;
+    email: string;
+    status: boolean;
+    created_at: Date;
+    updated_at: Date;
+}
+
+
+const CustomerSchema = new Schema<ICustomer>({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: Boolean,
+        required: true
+    },
+},
+{
+    collection: 'customers', //nome do schema/collection no mongodb
+    timestamps: { //Mongoose adiciona automaticamente
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+}
+)
+
+const Customer = mongoose.model<ICustomer>(
+    'Customer',
+    CustomerSchema
+);
+
+export default Customer
